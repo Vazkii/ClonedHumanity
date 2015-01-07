d6742f17468596d35282f5786c8a1b347a9a1a6a
Lizbeth Rika <vazkii@hotmail.com>
2015-01-07 22:18:55 +0000

    Rudimentary socket connection.

--- ch_main.js
+++ ch_main.js
@@ -1,9 +1,20 @@
+// =============== Modules Start
 var fs = require('fs');
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var util = require('util');
+// =============== Modules End
 
+// =============== Main Start
 var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 var port = config['port'];
+
+var GameServer = function() {
+	this.games = [];
+	this.players = {};
+};
+var gameserver = new GameServer();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/index.html');
@@ -12,3 +23,24 @@ app.get('/', function(req, res){
 http.listen(port, function() {
 	console.log('HTTP Server running on port ' + port);
 });
+
+io.on('connection', function(socket) {
+	gameserver.onConnect(socket);
+	
+	socket.on('disconnect', function() {
+		gameserver.onDisconnect(socket);
+	});
+});
+// =============== Main End
+
+// =============== GameServer start
+GameServer.prototype.onConnect = function(socket) {
+	console.log('Connection opened by socket with ID ' + socket.id);
+	this.players[socket.id] = socket;
+}
+
+GameServer.prototype.onDisconnect = function(socket) {
+	console.log('Connection closed socket with ID ' + socket.id);
+	delete this.players[socket.id];
+}
+// =============== GameServer end

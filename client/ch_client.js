e988c6b153253888de7593c06146f90f0ea8cfdc
Lizbeth Rika <vazkii@hotmail.com>
2015-01-08 16:46:47 +0000

    Fancy selector.

--- client/ch_client.js
+++ client/ch_client.js
@@ -9,6 +9,25 @@ $(function() {
 	populateCards();
 });
 
+$(".cah-card").click(function() {
+	var selected = $(this).hasClass('selected-card');
+	if(!selected) {
+		deselectCards();
+		$(this).addClass('selected-card');
+		var checkbox = $(this).find('.card-checkbox');
+		checkbox.fadeIn(400).css('display', 'inline-block');
+	}
+});
+
+function deselectCards() {
+	$('.selected-card').each(function(e) {
+		$(this).removeClass('selected-card');
+		var checkbox = $(this).find('.card-checkbox');
+		checkbox.fadeOut(400);
+	});
+}
+
+
 function setChatboxHeight() {
 	$('#chatbox').height($(document).height() - $('#playfield').height() - $('#the-navbar').height() - 40);
 }
@@ -16,9 +35,7 @@ $(window).resize(setChatboxHeight);
 
 function populateCards() {
 	$('.cah-card').each(function(e) {
-		var html = "<div class='card-text'>" + findSpaces($(this).attr('data-text')) + "</div><div class='card-deck'>" + $(this).attr('data-deck') + "</div>";
-		if(!$(this).hasClass('cah-card-black'))
-			html += "<div class='radio card-pick-radio'><label><input type='radio' name='picked-card'></label></div>";	
+		var html = "<div class='card-text'>" + findSpaces($(this).attr('data-text')) + "</div><div class='card-deck'><div class='checkbox card-checkbox'><label><input type='checkbox' disabled='true' checked='true'></label></div>" + $(this).attr('data-deck') + "</div>";
 	
 		$(this).html(html);
 	});
